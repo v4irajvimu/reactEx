2de809f0dec97ac8a2d1a99ce1b30ad1711c5807
v4irajvimu <viraj.vimu@gmail.com>
2017-10-01 21:32:22 +0530

    States

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {todos : ['Eat Subs', 'Fuck girls', 'Sleep well']};
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.props.msg}</h1>
         </header>
-        <p className="App-intro">
-          Age: {this.props.details.Age}
-          Gender: {this.props.details.Gender}
-          School: {this.props.details.School}
-        </p>
+        <div id="todo-list">
+          <h2>My To Do List - Viraj</h2>
+          <ul>
+            <li>{this.state.todos[0]}</li>
+            <li>{this.state.todos[1]}</li>
+            <li>{this.state.todos[2]}</li>
+          </ul>
+        </div>
       </div>
     );
-  }
+  } // render
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var myDetails = {
-  Age : '27',
-  Gender: 'Male',
-  School: 'SSCK'
-};
-ReactDOM.render(<App msg = "Viraj Vimukthi Jayasinghe." details = {myDetails}/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
